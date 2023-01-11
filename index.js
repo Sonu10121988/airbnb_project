c88e40af3d2cfe4cf3a642c30c8f5012f97048c9
sonuyadav <sonuyadavm2017@gmail.com>
2023-01-11 12:54:05 +0530

    add

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "../templates/views"));
+app.set("views", path.join(__dirname, "./templates/views"));
 
 const port = process.env.PORT || 3000;
 
